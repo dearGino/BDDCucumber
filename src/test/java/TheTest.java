3c450f0f64437eb7c1983cbb7bd0c3521de0ec34
Anna Persh <dearest.gino@gmail.com>
2022-10-30 01:33:41 +0500

    added form filling in scenario

--- src/test/java/TheTest.java
+++ src/test/java/TheTest.java
@@ -8,8 +8,16 @@ public class TheTest extends BaseTest {
 	 public void firstTest() throws InterruptedException {
 	
 		startTest.getHomePage()
+		.closeCookie()
 		.menuMortage()
-		.menuSecondHouseMortage();
+		.menuSecondHouseMortage()
+		.switchToMortageFrame()
+		.fillFieldByName("Стоимость недвижимости","5180000")
+		.fillFieldByName("Первоначальный взнос","3058000")
+		.fillFieldByName("Срок кредита","30")
+		.checkboxChangeStatus("Скидка на Домклик", false)
+		.checkboxChangeStatus("Страхование жизни и здоровья", false)
+		.checkboxChangeStatus("Электронная регистрация сделки", false);
 	}
 
 }
